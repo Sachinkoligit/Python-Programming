2fed0800f3df7feab8772e5ffa6a56b8b79c540b
SACHIN KOLI <132149727+Sachinkoligit@users.noreply.github.com>
2024-06-24 14:32:39 +0530

    Add files via upload

---
+++ Hangman/hangman.py
@@ -0,0 +1,42 @@
+import hangman_words
+import random
+import hangman_art
+
+print(hangman_art.logo)
+word_list=hangman_words.word_list
+chosen_word = random.choice(word_list)
+word_length = len(chosen_word)
+
+end_of_game = False
+lives = 6
+
+print(f'Pssst, the solution is {chosen_word}.')
+
+#Create blanks
+display = []
+for _ in range(word_length):
+    display += "_"
+
+while not end_of_game:
+    guess = input("Guess a letter: ").lower()
+
+    if guess in display :
+      print("You already Entered This Letter.")
+    for position in range(word_length):
+        letter = chosen_word[position]
+        if letter == guess:
+            display[position] = letter
+
+    if guess not in chosen_word:
+        lives -= 1
+        if lives == 0:
+            print("You lose.")
+            end_of_game = True
+            
+    print(f"{' '.join(display)}")
+
+    if "_" not in display:
+        end_of_game = True
+        print("You win.")
+
+    print(hangman_art.stages[lives])
